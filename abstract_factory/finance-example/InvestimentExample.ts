450092a62e463edc51eaa16cd161698007b0a661
victorjatoba <vmgjatoba@gmail.com>
2024-06-28 14:39:47 +0100

    finish finance abstract factory example

--- abstract_factory/finance-example/InvestimentExample.ts
+++ abstract_factory/finance-example/InvestimentExample.ts
@@ -16,9 +16,38 @@ class TaxCalculator {
     }
 }
 
-const calculator = new TaxCalculator("cryptocurrence")
+const calculator = new TaxCalculator("stock market")
 console.log(`sale percentage: ${calculator.sale}`)
 
 /**
  * Example using Abstract Factory
  */
+interface ActionRateFactory {
+    createSale(): string
+    createBuy(): string
+}
+
+class CryptoActionRateFactory implements ActionRateFactory {
+    createSale() {
+        return "0%"
+    }
+
+    createBuy() {
+        return "0%"
+    }
+}
+
+class TaxCalculatorAF {
+    sale: string
+    buy: string
+
+    constructor(factory: ActionRateFactory) {
+        this.sale = factory.createSale()
+        this.buy = factory.createBuy()
+    }
+}
+
+const cryptoActionRateFactory = new CryptoActionRateFactory()
+const calculatorAB = new TaxCalculatorAF(cryptoActionRateFactory)
+
+console.log(`sale rate: ${calculatorAB.sale}`)
